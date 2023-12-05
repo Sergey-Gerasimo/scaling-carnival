fe9e8521d9db6e2c15da81e82aafafd1040e03cd
Sergey-Gerasiomo <gerasimovsergey2004@gmail.com>
2023-12-06 01:36:04 +0300

    install: добавил установщик для windows

---
+++ install.bat
@@ -0,0 +1,7 @@
+python -m venv venv 
+venv/Scripts/pip install -r requirements.txt
+pyinstaller --onefile --noconsole --icon=logo_en.ico main.py
+mkdir bin
+xcopy dist/main.exe bin/scaling-carnaval.exe
+rd dist
+rd build
