dc99d92afcfb9e111bf2f67542268d436935e41d
Sergey-Gerasiomo <gerasimovsergey2004@gmail.com>
2023-12-06 01:22:19 +0300

    install: написал скрипт установки для MacOs и Linux

--- gui.py
+++ gui.py
@@ -5,15 +5,17 @@ from exceptions import IncorrectFileName, IncorrectFile, EmptyPath
 from readxl import read
 from output import create_work_book
 import io
+import os 
+from typing import final
 
 class Main(tk.Tk): 
 
     def __init__(self, *args, **kwargs) -> None: 
         super().__init__(*args, **kwargs)
         self.title("scaling-carnival")
-        self.__logo = tk.PhotoImage(file='logo_en.png')
+        self.__logo = tk.PhotoImage(file=f'/Users/andreigerasimov/code/scaling-carnival/images/logo_en.png')
         tk.Label(image=self.__logo).pack()
-        self.iconphoto(False, tk.PhotoImage(file='logo_en.png'))
+        self.iconphoto(False, tk.PhotoImage(file=f'/Users/andreigerasimov/code/scaling-carnival/images/logo_en.png'))
         self.path_to_file = Path('')
         self.path_to_save = Path("output.xlsx")
 
