7998d9e2592fb9f0da4e27addd0e804587992654
sergey <85431780+Sergey-Gerasimo@users.noreply.github.com>
2023-12-07 01:40:28 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,8 +1,12 @@
-python -m venv venv 
-venv/Scripts/pip install -r requirements.txt
-pyinstaller --onefile --noconsole --icon=logo_en.ico main.py
+mkdir python
+curl https://www.python.org/ftp/python/3.12.0/python-3.12.0-amd64.exe --output python\python-3.12.0.exe
+python\python-3.12.0.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0
+
+%LocalAppData%\Programs\Python\Python312\python -m venv venv
+venv\Scripts\pip install -r requirements.txt
+venv\Scripts\pyinstaller --onefile --noconsole --icon=logo_en.ico main.py
 mkdir bin
-xcopy dist/main.exe bin/scaling-carnaval.exe
-rd dist
-rd build
-set PATH=%PATH%;%CD%\bin
+copy dist\main.exe bin\scaling-carnaval.exe
+rd /s /q dist
+rd /s /q build
+rd /s /q %LocalAppData%\Programs\Python\Python312
